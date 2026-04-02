91a203911a82c1c2ac476fd9cf6e6d7fe1eead1c
mikledez <miwork546@gmail.com>
2026-04-02 15:47:57 +0800

    Created initial loop functin, need to configure correctU\ UI layout for 16x16

---
+++ script.js
@@ -0,0 +1,12 @@
+let size = "16x16";
+
+
+if (size == "16x16") {
+    for (i = 0; i < 256; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.className = "innerBox";
+
+        const parent = document.getElementById("container");
+        parent.appendChild(newDiv);
+    }
+}
