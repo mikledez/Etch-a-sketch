fbcb815f24e2d911cbb942e086ed2814dfcc6b09
mikledez <miwork546@gmail.com>
2026-04-03 22:06:02 +0800

    Finished everything including the UI size update, draws on mouse click instead, and removed grid borders

--- script.js
+++ script.js
@@ -23,28 +23,48 @@ function loadGrid(size) {
 
     document.documentElement.style.setProperty('--size', size);
 
-    for (i = 0; i < size * size; i++) {
+    for (let i = 0; i < size * size; i++) {
         const newDiv = document.createElement("div");
         newDiv.className = "innerBox";
 
-        newDiv.addEventListener('mouseenter', () => {
+        newDiv.addEventListener('mouseover', () => {
+            if (isDrawing == true) {
+                newDiv.style.backgroundColor = getRandomColor();
+            }
+
+        });
+        newDiv.addEventListener('mousedown', () => {
             newDiv.style.backgroundColor = getRandomColor();
         });
         container.appendChild(newDiv);
     }
 }
 
-//declarations
+//declarations 
+let isDrawing = false;
+
+window.addEventListener('mousedown', () => {
+    isDrawing = true;
+});
+
+window.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
 let size = 16;
 document.documentElement.style.setProperty('--size', `${size}`);
 loadGrid(size);
+
 
 const clearBtn = document.getElementById("clearBtn");
 const slider = document.getElementById("sizeSlider");
 
 
+
+
 slider.addEventListener('input', () => {
     size = slider.value;
+    document.getElementById("boxSize").textContent = size + "x" + size;
     loadGrid(size);
 });
 
