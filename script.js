6ee941daa760c4bcf3352e1643d95a6ab8b7b3d1
mikledez <miwork546@gmail.com>
2026-04-03 23:57:05 +0800

    Final Commit, added random and black modes, with opacity feature in black mode. Changed up the UI slightly

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function clearGrid() {
 
     box.forEach((div) => {
         div.style.backgroundColor = 'white';
+        div.dataset.opacity = 0;
     });
 }
 
@@ -26,22 +27,59 @@ function loadGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const newDiv = document.createElement("div");
         newDiv.className = "innerBox";
+        newDiv.dataset.opacity = 0;
 
         newDiv.addEventListener('mouseover', () => {
-            if (isDrawing == true) {
+            if (!isDrawing) return;
+
+            if (mode === "random") {
                 newDiv.style.backgroundColor = getRandomColor();
             }
 
+            if (mode === "black") {
+                let currentOpacity = Number(newDiv.dataset.opacity);
+
+                if (currentOpacity < 1) {
+                    currentOpacity += 0.1;
+                }
+
+                newDiv.dataset.opacity = currentOpacity;
+                newDiv.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity})`;
+            }
         });
+
         newDiv.addEventListener('mousedown', () => {
-            newDiv.style.backgroundColor = getRandomColor();
+            if (mode === "random") {
+                newDiv.style.backgroundColor = getRandomColor();
+            }
+
+            if (mode === "black") {
+                let currentOpacity = Number(newDiv.dataset.opacity);
+
+                if (currentOpacity < 1) {
+                    currentOpacity += 0.1;
+                }
+
+                newDiv.dataset.opacity = currentOpacity;
+                newDiv.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity})`;
+            }
         });
+
         container.appendChild(newDiv);
     }
 }
 
 //declarations 
 let isDrawing = false;
+
+let mode = "random";
+
+const clearBtn = document.getElementById("clearBtn");
+const randomBtn = document.getElementById("randomBtn");
+const blackBtn = document.getElementById("blackBtn");
+const slider = document.getElementById("sizeSlider");
+
+//-------------------------------
 
 window.addEventListener('mousedown', () => {
     isDrawing = true;
@@ -56,12 +94,6 @@ document.documentElement.style.setProperty('--size', `${size}`);
 loadGrid(size);
 
 
-const clearBtn = document.getElementById("clearBtn");
-const slider = document.getElementById("sizeSlider");
-
-
-
-
 slider.addEventListener('input', () => {
     size = slider.value;
     document.getElementById("boxSize").textContent = size + "x" + size;
@@ -73,4 +105,10 @@ slider.addEventListener('input', () => {
 clearBtn.addEventListener('click', function () {
     clearGrid();
 });
-//-----------
+
+randomBtn.addEventListener('click', function () {
+    mode = "random";
+});
+blackBtn.addEventListener('click', function () {
+    mode = "black"
+});
