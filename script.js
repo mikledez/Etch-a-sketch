d384b5ddffb7ed57b40db9da02127cc503ad8f32
mikledez <miwork546@gmail.com>
2026-04-03 20:52:26 +0800

    Added the clear function, last is to change size from UI

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ function getRandomColor() {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
+function clearGrid(box) {
 
+    box.forEach((div) => {
+        div.style.backgroundColor = 'white';
+    });
+    box.style.backgroundColor = 'white';
+}
 
+//declarations
 let newSize = 10;
 document.documentElement.style.setProperty('--size', `${newSize}`);
 
+const clearBtn = document.getElementById("clearBtn");
 
+
+//------------------
+
+
+//Load Grid Size block
 for (i = 0; i < newSize * newSize; i++) {
     const newDiv = document.createElement("div");
     newDiv.className = "innerBox";
@@ -20,13 +33,20 @@ for (i = 0; i < newSize * newSize; i++) {
     parent.appendChild(newDiv);
     console.log(i + 1);
 }
+//------------------
 
 const box = document.querySelectorAll('.innerBox');
-
-box.forEach((div, index) => {
-    console.log(`Div index: ${index}`, div);
-
+//Color box block
+box.forEach((div) => {
     div.addEventListener('mouseenter', () => {
         div.style.backgroundColor = getRandomColor();
     });
 });
+//-------------------
+
+
+//clear Grid block
+clearBtn.addEventListener('click', function () {
+    clearGrid(box);
+});
+//------
